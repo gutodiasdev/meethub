7a44fd477bcd73c4570d061c916cc65534f3dbe9
gutodiasux <gutodiasux@gmail.com>
2021-11-27 19:28:46 -0300

    Xô Sufoco

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,5 +1,6 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../src/services/errors/AuthTokenError";
 
 export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
@@ -14,6 +15,21 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
       }
     }
 
-    return await fn(ctx)
+    try {
+      return await fn(ctx)
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, 'meethub.token')
+        destroyCookie(ctx, 'meethub.refreshToken')
+
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      }
+    }
+
   }
 }
