9b3dff1299245cd3c4bb9465caf0856fe2895c3d
Augusto Dias <gutodiasux@gmail.com>
2021-12-16 09:21:39 -0300

    add mentor container component

--- src/pages/app/mentores/index.tsx
+++ src/pages/app/mentores/index.tsx
@@ -1,66 +1,34 @@
 import { Center, Box, Avatar, Heading, Flex, Text, HStack, Tag, Grid, GridItem } from '@chakra-ui/react'
 import AppContainer from "../../../components/AppContainer";
+import { MentorContainer } from '../../../components/MentorContainer/indext';
 import { setupAPIClient } from '../../../services/api';
 import { withSSRAuth } from '../../../utils/withSSRAuth';
 
 export default function Mentors({ mentors }) {
-
   return (
     <AppContainer>
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        w="100%"
-        gap={4}
-        p="4"
-      >
-        {mentors.map(mentor => {
-          if (mentor.permissions === "mentor") {
+      <Flex direction="column">
+        <Grid templateColumns="repeat(4, 1fr)">
+          {mentors.map(mentor => {
             return (
-              <GridItem columnSpan={1}>
-                <Flex
-                  as="a"
-                  direction="column"
-                  href={`mentores/${mentor.id}`}
-                  cursor="pointer"
-                  key={mentor.id}
-                  border="1px"
-                  borderColor="gray.100"
-                  borderRadius="8"
-                  p="4"
-                  align="center"
-                >
-                  <Center
-                    w="150px"
-                  >
-                    <Avatar size="xl" />
-                  </Center>
-                  <Box textAlign="center">
-                    <Tag size="sm" my="3">Marketing</Tag>
-                    <Heading
-                      size="md"
-                    >
-                      {mentor.name}</Heading>
-                    <Text
-                      color="gray.500"
-                      fontWeight="thin"
-                    >
-                      {mentor.position}</Text>
-                  </Box>
-                </Flex>
-              </GridItem>
+              <MentorContainer
+                mentorId={mentor.id}
+                mentorName={mentor.name}
+                mentorPosition={mentor.position}
+                // mentorCategories={mentor.dategories}
+              />
             )
-          }
-        })}
-      </Grid>
+          })}
+        </Grid>
+      </Flex>
     </AppContainer>
   )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
-  const response = await apiClient.get(`/users`);
-
-  console.log(response.data);
+  const response = await apiClient.get(`/mentors`);
+  const me = await apiClient.get(`/me`);
 
   return {
     props: {
