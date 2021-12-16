55575f98a3c1484d8c23c9dd665c7feea3e0965f
Augusto Dias <gutodiasux@gmail.com>
2021-12-16 09:25:25 -0300

    add relation users to categories

---
+++ prisma/migrations/20211216122326_add_categories_to_users/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_CategoryToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToUser_AB_unique" ON "_CategoryToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToUser_B_index" ON "_CategoryToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToUser" ADD FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToUser" ADD FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
