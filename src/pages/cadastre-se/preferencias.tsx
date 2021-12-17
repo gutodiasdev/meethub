bad9c34f990a7b5f11cdd76617e3b19c74527794
Augusto Dias <gutodiasux@gmail.com>
2021-12-17 08:27:13 -0300

    add fix preferencias

--- src/pages/cadastre-se/preferencias.tsx
+++ src/pages/cadastre-se/preferencias.tsx
@@ -1,50 +1,41 @@
-import { Flex, Input, Spinner, Text } from "@chakra-ui/react";
+import { Flex, Input, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { useCategories } from '../../services/hooks/categories/useCategories'
 
 export default function Preferences() {
-  const [preferences, setPreferences] = useState([])
-
-  const { data, isLoading } = useCategories()
+  const [preferences, setPreferences] = useState(['Uncategorized'])
 
   const categories = ['Vendas', 'Gestão', 'Marketing']
-
-  function handlePreferences(value) {
-    setPreferences(old => [...old, value.target.value])
-    localStorage.setItem('meethub.my.preferences', JSON.stringify(preferences))
-  }
-
+  
   useEffect(() => {
-    const data = localStorage.getItem('meethub.my.preferences')
+    const data = localStorage.getItem('meethub-preferences')
     if (data) {
-      setPreferences(Array(data));
+      setPreferences(JSON.parse(data))
     }
   }, [])
+  
+  function handlePreferences(value) {
+    setPreferences(old => [...old, value.target.value])
+    localStorage.setItem('meethub-preferences', JSON.stringify(preferences))
+  }
+
 
   return (
     <Flex maxW={600} mx="auto" alignItems="center" flexWrap="wrap">
-      {isLoading ? (
-        <Spinner />
-      ): (
-        data.map(category => {
-          return (
-            <Input
-              as="button"
-              mx='1'
-              w={150}
-              value={category.name}
-              key={category.id}
-              borderRadius="full"
-              onClick={handlePreferences}
-              flexBasis="auto"
-            >
-              {category.name}
-            </Input>
-          )
-        })
-      )}
-      {preferences.map(single => {
-        return <Text mx={2}>{single}</Text>
+      {categories.map((value, key) => {
+        return (
+          <Input
+            as="button"
+            mx='1'
+            maxW={180}
+            value={value}
+            key={key}
+            borderRadius="full"
+            onClick={handlePreferences}
+            flexBasis="auto"
+          >
+            {value}
+          </Input>
+        )
       })}
     </Flex>
   )
