110b0a296efb44d28d26ffbac83dede9880e89fb
Augusto Dias <gutodiasux@gmail.com>
2021-12-22 09:17:34 -0300

    add asMentor meet list with edit and exclude buttons

---
+++ src/pages/api/meets/my/index.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import nextConnect from 'next-connect'
+import prisma from '../../../../lib/utils/prisma'
+
+const handler = nextConnect<NextApiRequest, NextApiResponse>({
+  onNoMatch: () => { },
+  onError: () => { }
+}).post(async (req, res) => {
+  const { userId } = req.body
+
+  const mentor = await prisma.meet.findMany({
+    where: {
+      members: {
+        some: {
+          user: {
+            AND: [{
+              id: userId,
+            },
+            {
+              roles: 'mentor'
+            }
+            ]
+          }
+        }
+      }
+    }
+  })
+
+  return res.status(200).json({ asMentor: mentor })
+
+})
+
+export default handler
--- src/pages/app/meets/meus-meets/index.tsx
+++ src/pages/app/meets/meus-meets/index.tsx
@@ -1,173 +1,205 @@
 import AppContainer from "../../../../components/AppContainer";
 import { setupAPIClient } from "../../../../services/api";
 import { withSSRAuth } from "../../../../utils/withSSRAuth";
-import { Flex, Center, Avatar, Box, HStack, Heading, Tag, Text, Button, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
+import { Flex, Center, Avatar, Box, HStack, Heading, Tag, Text, Button, Tabs, TabList, Tab, TabPanels, TabPanel, Spinner, Icon } from '@chakra-ui/react'
 import { WhoCanUse } from "../../../../components/WhoCanUse";
+import { useQuery } from 'react-query'
+import { api } from "../../../../services/apiClient";
+import { RiAddFill, RiEditBoxLine } from "react-icons/ri";
+import { FiTrash } from "react-icons/fi";
 
-export default function MyMeets({ asUser, asMentor }) {
+export default function MyMeets({ user }) {
+  const { data, isLoading, error } = useQuery('meets', async () => {
+    const { data } = await api.post('/meets/my', { userId: user.id })
+    const meets = data.asMentor.map(meet => {
+      return {
+        id: meet.id,
+        image: meet.image,
+        meetDetails: meet.meetDetails,
+        name: meet.name,
+        price: meet.price,
+      }
+    })
+
+    return { asMentor: meets }
+  })
+
+  console.log(data)
 
   return (
     <AppContainer>
       <Flex w="100%">
         <WhoCanUse permissions={['user']}>
-        {/* {asUser.map(meetItem => {
-                return (
-                  <Flex
-                    key={meetItem.id}
-                    bg="white"
-                    border="1px"
-                    borderColor="gray.100"
-                    borderRadius="8"
-                    width="100%"
-                    p="4"
-                    mb="4"
+          {/* {asUser.map(meetItem => {
+            return (
+              <Flex
+                key={meetItem.id}
+                bg="white"
+                border="1px"
+                borderColor="gray.100"
+                borderRadius="8"
+                width="100%"
+                p="4"
+                mb="4"
+              >
+                <Center
+                  w="150px"
+                >
+                  <Avatar size="lg" />
+                </Center>
+                <Box
+                  flex="1"
+                  columnSpan={4}
+                  direction="column"
+                  h="100%"
+                >
+                  <HStack>
+                    <Tag size="sm">Marketing</Tag>
+                  </HStack>
+                  <Heading
+                    size="md"
+                    mt="2"
+                    mb="1"
                   >
-                    <Center
-                      w="150px"
-                    >
-                      <Avatar size="lg" />
-                    </Center>
-                    <Box
-                      flex="1"
-                      columnSpan={4}
-                      direction="column"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Tag size="sm">Marketing</Tag>
-                      </HStack>
-                      <Heading
-                        size="md"
-                        mt="2"
-                        mb="1"
-                      >
-                        {meetItem.name}</Heading>
-                      <Text
-                        color="gray.500"
-                        fontWeight="thin"
-                      >
-                        {meetItem.description}</Text>
-                    </Box>
-                    <Button
-                      as="a"
-                      target="_blank"
-                      href={`/app/meets/meus-meets/sala/${meetItem.id}`}
-                      colorScheme="blue"
-                    >
-                      Entrar na sala
-                    </Button>
-                  </Flex>
-                )
-              })} */}
+                    {meetItem.name}</Heading>
+                  <Text
+                    color="gray.500"
+                    fontWeight="thin"
+                  >
+                    {meetItem.description}</Text>
+                </Box>
+                <Button
+                  as="a"
+                  target="_blank"
+                  href={`/app/meets/meus-meets/sala/${meetItem.id}`}
+                  colorScheme="blue"
+                >
+                  Entrar na sala
+                </Button>
+              </Flex>
+            )
+          })} */}
         </WhoCanUse>
 
         <WhoCanUse roles={['mentor']}>
-        <Tabs w="100%">
-          <TabList>
-            <Tab>Como usuário</Tab>
-            <Tab>Como mentor</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              {/* {asUser.map(meetItem => {
-                return (
-                  <Flex
-                    key={meetItem.id}
-                    bg="white"
-                    border="1px"
-                    borderColor="gray.100"
-                    borderRadius="8"
-                    width="100%"
-                    p="4"
-                    mb="4"
-                  >
-                    <Center
-                      w="150px"
+          <Tabs w="100%">
+            <TabList>
+              <Tab>Como usuário</Tab>
+              <Tab>Como mentor</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                {/* {asUser.map(meetItem => {
+                  return (
+                    <Flex
+                      key={meetItem.id}
+                      bg="white"
+                      border="1px"
+                      borderColor="gray.100"
+                      borderRadius="8"
+                      width="100%"
+                      p="4"
+                      mb="4"
                     >
-                      <Avatar size="lg" />
-                    </Center>
-                    <Box
-                      flex="1"
-                      columnSpan={4}
-                      direction="column"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Tag size="sm">Marketing</Tag>
-                      </HStack>
-                      <Heading
-                        size="md"
-                        mt="2"
-                        mb="1"
+                      <Center
+                        w="150px"
                       >
-                        {meetItem.name}</Heading>
-                      <Text
-                        color="gray.500"
-                        fontWeight="thin"
+                        <Avatar size="lg" />
+                      </Center>
+                      <Box
+                        flex="1"
+                        columnSpan={4}
+                        direction="column"
+                        h="100%"
                       >
-                        {meetItem.description}</Text>
-                    </Box>
-                    <Button
-                      as="a"
-                      href={`sala/${meetItem.id}`}
-                      target="_blank"
-                      colorScheme="blue"
-                    >
-                      Entrar na sala
-                    </Button>
+                        <HStack>
+                          <Tag size="sm">Marketing</Tag>
+                        </HStack>
+                        <Heading
+                          size="md"
+                          mt="2"
+                          mb="1"
+                        >
+                          {meetItem.name}</Heading>
+                        <Text
+                          color="gray.500"
+                          fontWeight="thin"
+                        >
+                          {meetItem.description}</Text>
+                      </Box>
+                      <Button
+                        as="a"
+                        href={`sala/${meetItem.id}`}
+                        target="_blank"
+                        colorScheme="blue"
+                      >
+                        Entrar na sala
+                      </Button>
+                    </Flex>
+                  )
+                })} */}
+              </TabPanel>
+              <TabPanel>
+                <Flex justify='right'>
+                  <Button rightIcon={<RiAddFill />} colorScheme='blue' mb='8' >
+                    Adicionar novo
+                  </Button>
+                </Flex>
+                {isLoading ? (
+                  <Flex>
+                    <Spinner />
                   </Flex>
-                )
-              })} */}
-            </TabPanel>
-            <TabPanel>
-              {/* {asMentor.map(meetItem => {
-                return (
-                  <Flex
-                    cursor="pointer"
-                    key={meetItem.id}
-                    bg="white"
-                    border="1px"
-                    borderColor="gray.100"
-                    borderRadius="8"
-                    width="100%"
-                    p="4"
-                    mb="4"
-                  >
-                    <Center
-                      w="150px"
-                    >
-                      <Avatar size="lg" />
-                    </Center>
-                    <Box
-                      flex="1"
-                      columnSpan={4}
-                      direction="column"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Tag size="sm">Marketing</Tag>
-                      </HStack>
-                      <Heading
-                        size="md"
-                        mt="2"
-                        mb="1"
+                ) : (
+                  data.asMentor.map(meetItem => {
+                    return (
+                      <Flex
+                        justify='space-between'
+                        key={meetItem.id}
+                        bg="white"
+                        border="1px"
+                        borderColor="gray.100"
+                        borderRadius="8"
+                        width="100%"
+                        p="4"
+                        mb="4"
                       >
-                        {meetItem.name}</Heading>
-                      <Text
-                        color="gray.500"
-                        fontWeight="thin"
-                      >
-                        {meetItem.description}</Text>
-                    </Box>
-                  </Flex>
-                )
-              })} */}
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+                        <Flex mr='2' >
+                          <Box
+                            flex="1"
+                            columnSpan={4}
+                            direction="column"
+                            h="100%"
+                          >
+                            <HStack>
+                              <Tag size="sm">Marketing</Tag>
+                            </HStack>
+                            <Heading
+                              size="md"
+                              mt="2"
+                              mb="1"
+                            >
+                              {meetItem.name}</Heading>
+                            <Text
+                              color="gray.500"
+                              fontWeight="thin"
+                            >
+                              {meetItem.description}</Text>
+                          </Box>
+                        </Flex>
+                        <Box textAlign='right'>
+                          <Icon as={RiEditBoxLine} cursor='pointer' _hover={{ stroke: 'red' }} />
+                          <Icon as={FiTrash} cursor='pointer' ml='2' />
+                        </Box>
+                      </Flex>
+                    )
+                  })
+                )}
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </WhoCanUse>
 
-        
+
       </Flex>
     </AppContainer>
   )
@@ -178,6 +210,8 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const response = await apiClient.get(`/me`);
 
   return {
-    props: {}
+    props: {
+      user: response.data
+    }
   }
 })
